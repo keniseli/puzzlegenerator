128e7869dde0bf88575b1d946b87f6d91b7dac1c
Patrik Lustenberger <studium@192.168.192.10>
2015-09-21 21:49:15 +0200

    fixed bug on screen steps

--- puzzlegenerator/puzzlegenerator_domain/src/main/java/com/flurnamenpuzzle/generator/Steps.java
+++ puzzlegenerator/puzzlegenerator_domain/src/main/java/com/flurnamenpuzzle/generator/Steps.java
@@ -9,8 +9,8 @@ public enum Steps {
 	STEP_1("stateSelection", PuzzleGeneratorConfig.STEPS_1_IMAGE), STEP_2(
 			"fieldNameMapSelection", PuzzleGeneratorConfig.STEPS_2_IMAGE), STEP_3(
 			"verificationAndDecision", PuzzleGeneratorConfig.STEPS_3_IMAGE), STEP_4(
-			"progressPanel", PuzzleGeneratorConfig.STEPS_4_IMAGE), STEP_5(
-			"targetPathPanel", PuzzleGeneratorConfig.STEPS_5_IMAGE), STEP_6(
+			"progressPanel", PuzzleGeneratorConfig.STEPS_3_IMAGE), STEP_5(
+			"targetPathPanel", PuzzleGeneratorConfig.STEPS_4_IMAGE), STEP_6(
 			"resultPanel", PuzzleGeneratorConfig.STEPS_5_IMAGE);
 
 	private String id;
